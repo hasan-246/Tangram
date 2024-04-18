c534a589a8f09df51e6556edfa26476f58312419
hasan-246 <tanjilsiam14@gmail.com>
2024-04-19 00:43:07 +0600

    test

--- src/main/java/com/example/tangramtest/HelloApplication.java
+++ src/main/java/com/example/tangramtest/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start( Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
         Parent root = loader.load();
         HelloController controller = loader.getController();
